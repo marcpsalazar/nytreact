3fd098e6c99d517840d315100ca8f3bd50a38012
marcpsalazar@gmail.com <Patrick@Traceys-MacBook.local>
2018-07-20 11:15:31 -0400

    ff

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mern");
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   const path = require('path');
@@ -8,15 +11,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-
-const mongoose = require('mongoose');
-
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/mern",
-  {
-    useMongoClient: true
-  }
-);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);

a46218337b9d3ffd18c332eb01ec44addb315807
marcpsalazar@gmail.com <Patrick@Traceys-MacBook.local>
2018-07-23 23:26:17 -0400

    almost working

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,27 +1,21 @@
 import axios from "axios";
 
 const api = {
-  // Query NYT API
   searchNYT: function(topic, startYear, endYear) {
     const authKey = "996e3a9b63494feeb85afbd1e74b7c71";
     const queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
     authKey + "&q=" + topic + "&begin_date=" + startYear + "0101&end_date=" + endYear + "1231";
-    console.log(queryURL);
-    return axios.get(queryURL);
-
+  console.log(queryURL);
+  return axios.get(queryURL);
   },
-  // Retrieves saved articles from the db
   getArticle: function() {
     return axios.get("/api/article");
   },
-  // Saves a new article to the db
   saveArticle: function(articleObj) {
     return axios.post("/api/articles", articleObj);
   },
-  // Deletes an article from the db
   deleteArticle: function(id) {
     return axios.delete(`/api/articles/${id}`);
   }
 };
-
 export default api;
